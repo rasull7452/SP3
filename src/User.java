e799e774b043aceb581632337479209b79474af7
rasull7452 <rasullhossein250@gmail.com>
2025-11-10 00:49:45 +0100

    tilføjet fileio, app class og user class

---
+++ src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    void createUser(String username, String password){
+
+    }
+}
---
+++ src/media/Movie.java
@@ -0,0 +1,8 @@
+package media;
+
+public class Movie extends Media {
+
+    public Movie(String title, String category, int duration, int rating, int releaseYear){
+        super(title, category, duration, rating, releaseYear);
+    }
+}
